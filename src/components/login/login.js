a4f45905f7ae3b4f3d856e767762c9dd6082bbfa
ViliusRusinas <vilius.rusinas@stud.vilniustech.lt>
2022-11-28 18:24:51 +0200

    Front-end-ip

---
+++ src/components/login/login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function Login() {
+    return(
+        <nav className="bg-dark navbar-dark navbar">
+            <div className="row col-12 d-flex justify-content-center text-white">
+                <h3>Login</h3>
+            </div>
+        </nav>
+    )
+}
+export default Login;
