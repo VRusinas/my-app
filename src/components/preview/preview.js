3d7005b4fb64bb8a78f2d2ff9ba74bcff7f0292d
ViliusRusinas <vilius.rusinas@stud.vilniustech.lt>
2022-12-11 11:03:31 +0200

    preview

--- src/components/preview/preview.js
+++ src/components/preview/preview.js
@@ -4,6 +4,35 @@ import './preview.css'
 
 function Preview() {
     const [orderName, setOrderName] = useState([]);
+    const [previewData, setpreviewData] = useState([]);
+
+
+    function initLogin(){
+        const requestOptions = {
+            method: 'GET',
+            headers : { 
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+               },
+            body: JSON.stringify({
+                previewId: orderName
+            })
+        };
+        fetch('http://localhost:8080/orders/id?' + new URLSearchParams({id: orderName}))
+            .then((response) => response.json())     
+            .then((response) => {
+                
+                setpreviewData(response);
+                // for(let i; i < response.length; i++){
+
+                // }
+       
+    })       
+    }
+
+    initLogin();
+
+
 
     useEffect(()=>{
        
@@ -13,16 +42,32 @@ function Preview() {
         console.log(orderName);
 
     return(
-        <div className='preview-conatiner'>
-            <div className='preview-body'>
-                <h1>Order preview</h1>
-                    <div className='order-preview-container'>
+        <div>
+            
+                
+                <h1>Your order list:</h1>
+        
+                
+            
+            
+                    {previewData.map((dat, index) => (
+            
+                        <form key={index} className='formClass' >
+                            <><label className='labelT'>Id  <input className='inputCl'  readOnly={true} value={dat.id} name="name" /></label>
+                            <label className='labelT'>Data  <input className='inputCl'  readOnly={true} value={dat.deliveryDate}  /></label>
+                            <label className='labelT'>Order name  <input className='inputCl'  readOnly={true} value={dat.orderNumber} /></label>
+                            <label className='labelT'>Order state  <input className='inputCl'  readOnly={true} value="In progress"  /></label>
+                            <div className='button-container'>
 
-                        <p>{orderName}</p>
-                    </div>
+
+                                </div></>
+                        
+                        </form>
+                        ))}
+                
+            
 
             </div>
-        </div>
     )
 }
 export default Preview;
