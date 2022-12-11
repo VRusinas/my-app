6d47c0bd5f43d57c0d8da1f7eb07019910e958dc
ViliusRusinas <vilius.rusinas@stud.vilniustech.lt>
2022-12-11 16:31:42 +0200

    edittttt

--- src/components/edit/edit.js
+++ src/components/edit/edit.js
@@ -1,11 +1,102 @@
-import React from 'react';
+import React,  {useState,setState, useEffect}  from 'react';
+import UserTable from '../userTable/userTable';
 function Edit() {
-    return(
-        <nav className="bg-dark navbar-dark navbar">
-            <div className="row col-12 d-flex justify-content-center text-white">
-                <h3>Edit</h3>
-            </div>
-        </nav>
-    )
+
+
+    const [editData, setEditData] = useState([]);
+    const [editclk, setedtClick] = useState([]);
+    const [editrequirements, setEditRequirements] = useState(null);
+
+
+
+    function initEdit(){
+        const requestOptions = {
+            method: 'GET',
+            headers : { 
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+               },
+            body: JSON.stringify({
+                previewId: editData
+            })
+        };
+        fetch('http://localhost:8080/orders/id?orderPreviewId=' +  window.orderId)
+            .then((response) => response.json())     
+            .then((response) => {
+                
+                setEditData(response);
+                console.log(editData);
+                // for(let i; i < response.length; i++){
+
+                // }
+       
+    })       
+    }
+    
+    function initEditPost(){
+        const requestOptions = {
+            method: 'POST',
+           // headers: { 'Content-Type': 'application/json' },
+            headers : { 
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+               },
+            body: JSON.stringify({
+                id: window.orderId,
+                requirements: editrequirements
+            })
+        };
+        fetch('http://localhost:8080/orders/id', requestOptions,)
+            
+    }
+
+
+
+    useEffect(()=>{
+        initEdit();
+        }, [])
+
+        const handleClick = (event) =>{
+            initEditPost()
+
+            
+            let clk = 1;
+            setedtClick(clk++);
+        }
+
+       
+    const handleInputChange = (e) => {
+        const {id , value} = e.target;
+        if(id === "requirements"){
+            setEditRequirements(value);
+        }
+    }
+
+
+        if(editclk < 1){
+        return(
+      
+            <div className='preview-main'>
+                
+                    <form  className='form-preview' >
+                    <div className='headerPrw'>
+                    <h1>Your order information</h1>
+                    <p  onClick={event => handleClick(event)} className='prwBtn'>Save</p>
+                    </div>
+                
+                        <>
+            
+
+
+                        <div className='big-data'>
+                        <p>Order requirements</p>
+                        <label className='lb-bg'> <input type="text" className='prw-impt-bg' value="wsdw" onChange = {(e) => handleInputChange(e)}   /></label>
+                        </div></>
+                    
+                    </form>
+        </div>
+        )
+    }   
+    else return (<UserTable/>)
 }
 export default Edit;
